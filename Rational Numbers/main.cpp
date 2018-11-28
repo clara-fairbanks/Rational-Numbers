b6bdb76b10c0560dfdb5c2fef8783201208e11f5
Clara Fairbanks <clarafairbanks@claras-macbook-pro.local>
2018-11-27 21:04:30 -0500

    Initial Commit

---
+++ Rational Numbers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Rational Numbers
+//
+//  Created by Clara Fairbanks on 11/27/18.
+//  Copyright © 2018 Clara Fairbanks. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
